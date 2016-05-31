fbc25b7d6d3812d16d0c0a6870d9b926b1fa44d5
Ilya Novozhilov <Ilya_Novozhilov@epam.com>
2016-05-31 15:04:16 +0300

    add comparator

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,26 +1,18 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class Demo {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<Integer>();
+        List<Person> list = new ArrayList<Person>();
 
-        list.add(12);
-        list.add(13);
-        list.add(15);
-        list.add(14);
+        list.add(new Person(12));
+        list.add(new Person(13));
+        list.add(new Person(14));
+        list.add(new Person(15));
 
-        int i = CollectionUtil.findMax(list, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                if (o1 == o2) {
-                    return 0;
-                } else {
-                    return o1 - o2;
-                }
-            }
-        });
-        System.out.println("Max number is " + i);
+        PersonCompare personCompare = new PersonCompare();
+
+        Person p = CollectionUtil.findMax(list, personCompare);
+        System.out.println("Max age is " + p.getAge());
     }
 }
